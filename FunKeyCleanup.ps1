e51f31e1a11540478c8cd607de4ac399127d8dcd
Charlie Quick <cquickreal00@gmail.com>
2022-10-27 22:42:53 -0400

    Update FunKeyCleanup.ps1
    Changed script to make use of the new FileList.txt file so that it ONLY removes files I include in the text file (which should always only be the default included files).

--- FunKeyCleanup.ps1
+++ FunKeyCleanup.ps1
@@ -1,38 +1,17 @@
 # This is a short script to remove everything that comes in the console directories on FunKey OS by default
-# To use this script, place it in the root of the FunKey directory (the folder where you see all the console directories), right click it, hit "Run with PowerShell"
+# To use this script, place it and the FileList.txt file in the root of the FunKey directory (the folder where you see all the console directories), right click it, hit "Run with PowerShell"
 # Obviously there is much more that could be added to this, but it would be severe overkill for what the end goal is
 
 # Variables
 $cleanupCheck = Read-Host "Are you sure you want to clean out the default included game set? (y/n)"
-$dirs = @("Atari Lynx", "Game Boy", "Game Boy Advance", "Game Boy Color", "Game Gear", "Neo Geo Pocket", "NES", "PCE-TurboGrafx", "PS1", "Sega Genesis", "Sega Master System", "SNES", "WonderSwan")
-$biosFiles = @()
-$files = @()
-$location = Get-Location
-
-# Set location so that Get-ChildItem actually works on $dirs since it's not using full paths
-Set-Location $location
+$files = Get-Content -Path "FileList.txt"
 
 # Check if the user actually wants to run the script (y)
 if ($cleanupCheck.ToLower() -eq "y") {
-    foreach($dir in $dirs){
-        Write-Host "`n`nNow cleaning the $dir directory..." -ForegroundColor Green
-        Write-Host "Files Removed:"
-        $files += Get-ChildItem -Path $dir
-        foreach($file in $files){
-            if ($null -ne (Get-ChildItem -Path $file)) {
-                $biosFiles += Get-ChildItem -Path $file
-                 foreach($biosFile in $biosFiles){
-                    Remove-Item -Path $biosFile -Force
-                    Write-Host "$biosFile"
-                }
-                $biosFiles = @()
-            }
-            else {
-                Remove-Item -Path $file -Force
-                Write-Host "$file"
-            }
-        }
-        $files = @()
+    Write-Host "`nFiles Removed:" -ForegroundColor Green
+    foreach ($file in $files) {
+        Remove-Item -Path $file -Force
+        Write-Host "$file"
     }
 }
 
@@ -46,4 +25,3 @@ else {
     "Input not valid: Script restarting..."
     .\"FunKeyCleanup.ps1"
 }
-
